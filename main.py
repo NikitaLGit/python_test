731032cdf7354f6e9dec650aa1a6f720b28acead
NikitaLGit <nikitaleonovgit@gmail.com>
2022-03-30 15:10:10 +0300

    programm for calculating mass

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("hello nikitawin7")
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 4]
 
-x=5
-print(x)
+def lens(chis):
+    i = 0
+    for N in a:
+        if N<chis:
+            print(N)
+            i +=1
+        else:
+            continue
+
+c = int(input("Vvedite chislo: "))
+lens(c)
