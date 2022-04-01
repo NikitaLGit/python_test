7d82786d25cd4660e97248cc3d7406f8c3e612e2
Nikita Leonov <nikitaleonovgit@gmail.com>
2022-04-01 16:01:32 +0300

    what

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 4]
 
+
 def lens(chis):
     i = 0
     for N in a:
-        if N<chis:
+        if N < chis:
             print(N)
-            i +=1
+            i += 1
         else:
             continue
+
 
 c = int(input("Vvedite chislo tut: "))
 lens(c)
